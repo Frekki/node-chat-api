3604bac814c1dbe98e152402ee1a1b10f56ec505
Frekki <sobolrip1@gmail.com>
2017-08-04 13:44:16 +0300

    Some changes

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(email) {
-  console.log('newMessage', email);
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
 });
