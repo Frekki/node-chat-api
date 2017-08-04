9f62ca73b911bc32e81425acd8ad453a6971608e
Frekki <sobolrip1@gmail.com>
2017-08-04 13:30:40 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('userEmail', {
-    from: 'dady@example.com',
-    text: 'Who is your dady...',
-    createdAt: 44523
-  });
-
-  socket.on('cEmail', (newEmail) => {
-    console.log('cEmail', newEmail);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
